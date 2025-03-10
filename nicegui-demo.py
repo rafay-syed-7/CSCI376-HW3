09b048e70b6c1bf0925150069d88ddee6115869c
Rafay <rms7@williams.edu>
2025-03-10 11:49:35 -0400

    tried again

--- nicegui-demo.py
+++ nicegui-demo.py
@@ -4,4 +4,8 @@ with ui.row().classes("mx-auto"):
     with ui.card():
         input_field = ui.input(on_change = lambda e: result.set_text(e.value.lower()))
         result = ui.label()
+
+    with ui.card():
+         ui.label("Temperature Converter").classes("text-2xl font-bold text-accent mb-4")
+    # text-2xl: 
 ui.run()
